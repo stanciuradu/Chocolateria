7b50d7252a5b809de8e8acae5dbbdbbb4ced4527
radu <radustanciu841@yahoo.ro>
2021-04-24 13:50:32 +0300

    first initialiaze for website

---
+++ script.js
@@ -0,0 +1,7 @@
+const button= document.getElementById('button');
+function changeColor(){
+  button.style.background='red';
+  button.style.width='170px';
+  button.style.height='40px';
+}
+button.addEventListener('mouseover',changeColor);
