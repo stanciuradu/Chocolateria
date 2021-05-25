949e6c543a9cb602c632253fd99746cb8658dfb0
radu <radustanciu841@yahoo.ro>
2021-05-25 17:13:51 +0300

    Format Prettier

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const button= document.getElementById('button');
-function changeColor(){
-  button.style.background='red';
-  button.style.width='170px';
-  button.style.height='40px';
+const button = document.getElementById("button");
+function changeColor() {
+  button.style.background = "red";
+  button.style.width = "170px";
+  button.style.height = "40px";
 }
-button.addEventListener('mouseover',changeColor);
+button.addEventListener("mouseover", changeColor);
